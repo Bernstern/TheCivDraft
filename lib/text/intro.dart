5ed67e2e0d0db8bb5a5ef903a4d786b35c81d73b
RyanD <RyanDomanski3@gmail.com>
2022-12-10 02:48:15 -0700

    necessary improvement

--- lib/text/intro.dart
+++ lib/text/intro.dart
@@ -1,2 +1,2 @@
 const String introText =
-    "Welcome to the Civapalooza Winter Games Draft. We need to ask some questions to get your team setup before advancing to the bans stage and the draft itself. Happy Drafting. ";
+    "Welcome to the Civapalooza Winter Games Draft. We need to ask some questions to get your team setup before advancing to the bans stage and the draft itself. Happy drafting. ";
