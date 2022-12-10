a2ef07380902f72d6132f6a0f2ac383ba6c39570
Bernie Conrad <charlie.conrad16@gmail.com>
2022-12-10 10:08:23 -0800

    Testing webhook

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ class SetupPage extends StatelessWidget {
         child: FractionallySizedBox(
           alignment: Alignment.center,
           widthFactor: 0.5,
-          child: RoundedBox(text: introText),
+          child: NumberPicker(update: () => {}, text: "Test", defaultValue: 3, min: 1, max: 3),
         ),
       ),
     );
@@ -76,6 +76,70 @@ class RoundedBox extends StatelessWidget {
         color: theme.primaryColorDark,
       ),
       child: Text(text, style: mediumTextStyle),
+    );
+  }
+}
+
+/*
+  Stateful widget that takes which a update function to change 
+  as well as the text to display widget.
+*/
+class NumberPicker extends StatefulWidget {
+  const NumberPicker({
+    Key? key,
+    required this.update,
+    required this.text,
+    required this.defaultValue,
+    required this.min,
+    required this.max,
+  }) : super(key: key);
+
+  final Function update;
+  final String text;
+  final int defaultValue;
+  final int min;
+  final int max;
+
+  @override
+  _NumberPickerState createState() => _NumberPickerState();
+}
+
+class _NumberPickerState extends State<NumberPicker> {
+  int _value = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _value = widget.defaultValue;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(widget.text, style: mediumTextStyle),
+        const SizedBox(width: 8.0),
+        IconButton(
+          icon: const Icon(Icons.remove),
+          onPressed: () {
+            setState(() {
+              _value = math.max(_value - 1, widget.min);
+              widget.update(_value);
+            });
+          },
+        ),
+        Text(_value.toString(), style: mediumTextStyle),
+        IconButton(
+          icon: const Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              _value = math.min(_value + 1, widget.max);
+              widget.update(_value);
+            });
+          },
+        ),
+      ],
     );
   }
 }
