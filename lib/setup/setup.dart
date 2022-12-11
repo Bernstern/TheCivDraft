caad9d7831bb4a72b001aabe7ab3745fd9d56ed7
Bernie Conrad <charlie.conrad16@gmail.com>
2022-12-10 22:52:10 -0800

    Added theming to hint buttons

--- lib/setup/setup.dart
+++ lib/setup/setup.dart
@@ -11,10 +11,11 @@ import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 import 'package:civgen/text/intro.dart';
 import 'package:civgen/models.dart';
 
-class SetupContainer extends StatelessWidget {
-  const SetupContainer({Key? key, required this.child}) : super(key: key);
+class BasicContainer extends StatelessWidget {
+  const BasicContainer({Key? key, required this.child, required this.color}) : super(key: key);
 
   final Widget child;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +27,7 @@ class SetupContainer extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(8.0),
-          color: theme.primaryColorDark,
+          color: color,
         ),
         child: child,
       ),
@@ -42,7 +43,8 @@ class SetupButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SetupContainer(
+    return BasicContainer(
+      color: theme.hintColor,
       child: TextButton(
         onPressed: () => onPressed(),
         child: Text(text, style: mediumTextStyle),
@@ -59,12 +61,13 @@ class RoundedBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SetupContainer(
+    return BasicContainer(
+        color: theme.primaryColorDark,
         child: Text(
-      text,
-      style: mediumTextStyle,
-      textAlign: TextAlign.center,
-    ));
+          text,
+          style: mediumTextStyle,
+          textAlign: TextAlign.center,
+        ));
   }
 }
 
@@ -105,7 +108,8 @@ class _NumberPickerState extends State<NumberPicker> {
 
   @override
   Widget build(BuildContext context) {
-    return SetupContainer(
+    return BasicContainer(
+      color: theme.primaryColorDark,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -197,11 +201,11 @@ class _SetupPageState extends State<SetupPage> {
     // the distance from the active card index
     for (int i = 0; i < _maxCardIndex; i++) {
       setupCards[i] = AnimatedSlide(
-        duration: const Duration(milliseconds: 500),
+        duration: const Duration(milliseconds: 1000),
         curve: Curves.easeInOut,
         offset: Offset(0, 1.5 * (i - _activeCardIndex)),
         child: AnimatedOpacity(
-            duration: const Duration(milliseconds: 500),
+            duration: const Duration(milliseconds: 1000),
             curve: Curves.easeInOut,
             opacity: i < _activeCardIndex ? .5 : (i == _activeCardIndex ? 1 : 0),
             child: setupCards[i]),
@@ -256,17 +260,18 @@ class _SetupPageState extends State<SetupPage> {
                   ),
                 ])),
           ),
+          // TODO: Make this fade out instead of just disappearing
           Visibility(
             visible: _showIntro,
             child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              FractionallySizedBox(widthFactor: 0.75, child: RoundedBox(text: introText)),
-              TextButton(
+              const FractionallySizedBox(widthFactor: 0.75, child: RoundedBox(text: introText)),
+              SetupButton(
                   onPressed: () => {
                         setState(
-                          () => {this._showIntro = false},
+                          () => {_showIntro = false},
                         )
                       },
-                  child: RoundedBox(text: "Start Drafting")),
+                  text: "Start Drafting"),
             ]),
           ),
         ],
